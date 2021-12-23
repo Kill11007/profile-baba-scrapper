0e11a623050ca0c7cb2a56f9c127eeff3999f737
mayank <mj112295@gmail.com>
2021-12-23 17:29:22 +0530

    deploy heroku

--- ProfleBabaDataApi/views.py
+++ ProfleBabaDataApi/views.py
@@ -39,7 +39,7 @@ def fetch_driver():
     opts.headless = True
 
     # Setting up driver
-    driver_path = Service(r"chromedriver_win32\chromedriver.exe")
+    driver_path = Service(r"../chromedriver_win32/chromedriver.exe")
     my_driver = webdriver.Chrome(service=driver_path, options=opts)
 
     return my_driver
